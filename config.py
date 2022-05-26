5a2d3b5845b3d2292ebf5b754b14652c88deaec3
Denis Polyakov <p-ov.da@ya.ru>
2022-05-26 17:09:33 +0300

    Переместил объявление переменных окружения в main()

--- config.py
+++ config.py
@@ -1,17 +1,2 @@
-import os
 
-from environs import Env
 
-env = Env()
-env.read_env()
-
-BASE_DIR = os.path.dirname(__file__) or '.'
-PATH_TO_LOGS = os.path.join(BASE_DIR, 'logs', 'logs.log')
-TELEGRAM_API_TOKEN = env("TELEGRAM_API_TOKEN")
-TELEGRAM_CHAT_ID = env("TELEGRAM_CHAT_ID")
-DEVMAN_API_TOKEN = env("DEVMAN_API_TOKEN")
-LONG_POLLING_URL = 'https://dvmn.org/api/long_polling/'
-SLEEP_TIME = env.int('SLEEP_TIME', 90)
-HEADERS = {
-    "Authorization": f"Token {DEVMAN_API_TOKEN}"
-}
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import asyncio
+import os
 
 import requests
 import telegram
+from environs import Env
 from loguru import logger
 
-from config import TELEGRAM_API_TOKEN, TELEGRAM_CHAT_ID, LONG_POLLING_URL, HEADERS, PATH_TO_LOGS, SLEEP_TIME
+BASE_DIR = os.path.dirname(__file__) or '.'
+PATH_TO_LOGS = os.path.join(BASE_DIR, 'logs', 'logs.log')
 
 
-async def get_review(processed_response: dict):
+async def get_review(processed_response: dict, telegram_api_token: str, telegram_chat_id: str):
     content = processed_response.get("new_attempts")[0]
     is_negative = content.get("is_negative")
     lesson_title = content.get("lesson_title")
@@ -15,36 +18,48 @@ async def get_review(processed_response: dict):
     valid_review_message = "К сожалению в работе нашлись ошибки." if is_negative else "Преподавателю все понравилось. \
     Можно приступать к следующему уроку."
     message = f"У Вас проверили работу '{lesson_title}'.\n\n{valid_review_message}\nСсылка на урок: {lesson_url}"
-    await send_message(message)
+    await send_message(message, telegram_api_token, telegram_chat_id)
 
 
 async def main():
+    env = Env()
+    env.read_env()
+
+    telegram_api_token = env("TELEGRAM_API_TOKEN")
+    telegram_chat_id = env("TELEGRAM_CHAT_ID")
+    devman_api_token = env("DEVMAN_API_TOKEN")
+    long_polling_url = 'https://dvmn.org/api/long_polling/'
+    sleep_time = env.int('SLEEP_TIME', 90)
+    headers = {
+        "Authorization": f"Token {devman_api_token}"
+    }
     timestamp = None
+
     while True:
         try:
             payload = {'timestamp': timestamp}
-            response = requests.get(LONG_POLLING_URL, headers=HEADERS, params=payload)
+            response = requests.get(long_polling_url, headers=headers, params=payload)
             response.raise_for_status()
             processed_response = response.json()
             timestamp = processed_response.get("timestamp_to_request")
             status = processed_response.get("status")
         except requests.exceptions.ReadTimeout:
             logger.debug(f'Сервер не ответил...Ждем и посылаем запрос еще раз.')
-            await asyncio.sleep(SLEEP_TIME)
+            await asyncio.sleep(sleep_time)
             continue
         except requests.exceptions.ConnectionError:
             logger.debug(f'Потеряно соединение...Ждем подключения.')
-            await asyncio.sleep(SLEEP_TIME)
+            await asyncio.sleep(sleep_time)
             continue
         if status == 'found':
             timestamp = processed_response.get("new_attempts")[0].get("timestamp")
-            await get_review(processed_response)
+            await get_review(processed_response, telegram_api_token, telegram_chat_id)
 
 
-async def send_message(msg: str):
-    bot = telegram.Bot(TELEGRAM_API_TOKEN)
+async def send_message(msg: str, telegram_api_token: str, telegram_chat_id: str):
+    bot = telegram.Bot(telegram_api_token)
     async with bot:
-        await bot.send_message(text=msg, chat_id=TELEGRAM_CHAT_ID)
+        await bot.send_message(text=msg, chat_id=telegram_chat_id)
 
 
 if __name__ == '__main__':
